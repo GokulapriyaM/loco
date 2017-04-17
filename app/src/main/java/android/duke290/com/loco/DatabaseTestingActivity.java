94de86dd4dc751fdee1e01885569aea5b58f6b4e
Fred Xu <fredxu2015@gmail.com>
2017-04-16 22:13:40 -0400

    Added referencelist class

--- app/src/main/java/android/duke290/com/loco/DatabaseTestingActivity.java
+++ app/src/main/java/android/duke290/com/loco/DatabaseTestingActivity.java
@@ -1,19 +1,31 @@
 package android.duke290.com.loco;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DatabaseTestingActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private Button uploadButton, getButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_testing);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        uploadButton = (Button) findViewById(R.id.Database_testing_upload_button);
+        getButton = (Button) findViewById(R.id.Database_testing_get_button);
     }
 
 
---
+++ app/src/main/java/android/duke290/com/loco/ReferenceList.java
@@ -0,0 +1,10 @@
+package android.duke290.com.loco;
+
+public class ReferenceList {
+    public double latitude;
+    public double longitude;
+    public String address;
+    public String type;
+    public String path;
+
+}
