7b51bc80f7bad3588397e883f9a2171c31e0124f
Kevin Kuo <kevin1996us@gmail.com>
2017-04-16 21:55:12 -0400

    added layout for database testing

---
+++ app/src/main/java/android/duke290/com/loco/DatabaseTestingActivity.java
@@ -0,0 +1,30 @@
+package android.duke290.com.loco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DatabaseTestingActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_testing);
+    }
+
+
+
+
+
+    protected void databaseTestingUploadClick(View button) {
+
+    }
+
+    protected void databaseTestingGetClick(View button) {
+
+    }
+}
