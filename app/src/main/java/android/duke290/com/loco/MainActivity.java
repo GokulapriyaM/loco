510e3b35e341c76fd9e2e83c42bee7ed66ac0ca2
Kevin Kuo <kevin1996us@gmail.com>
2017-04-29 23:08:15 -0400

    i will kill wendy

--- app/src/main/java/android/duke290/com/loco/MainActivity.java
+++ app/src/main/java/android/duke290/com/loco/MainActivity.java
@@ -376,8 +376,25 @@ public class MainActivity extends AppCompatActivity implements DatabaseFetchCall
             // or an error message sent from the intent service.
             mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
 
-            // edit address string so that it is just one line
-            mAddressOutput = mAddressOutput.replace("\r\n", ", ").replace("\n", ", ");
+//            // edit address string so that it is just one line
+//            mAddressOutput = mAddressOutput.replace("\r\n", ", ").replace("\n", ", ");
+            // edit address string so that it is just two lines
+            String edited_adr = "";
+            int cnt = 0;
+            for (int k1 = 0; k1 < mAddressOutput.length(); k1++) {
+                if (mAddressOutput.charAt(k1) == 10) {
+                    cnt++;
+                    if (cnt > 1) {
+                        edited_adr += ", ";
+                    } else {
+                        edited_adr += (char) 10;
+                    }
+                } else {
+                    edited_adr += mAddressOutput.charAt(k1);
+                }
+            }
+
+            mAddressOutput = edited_adr;
             displayAddressOutput();
 
             Log.d(TAG, "address found");
